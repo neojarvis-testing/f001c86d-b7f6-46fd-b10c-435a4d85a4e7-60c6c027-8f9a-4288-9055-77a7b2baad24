fa96d5f5b834c5f18db2c522aafbdfa3a5adf775
sumayyasultana5 <sumaayasultana898@gmail.com>
2025-04-21 21:11:37 +0000

    Day1

---
+++ Project/src/test/java/pages/WatchesPage.java
@@ -0,0 +1,135 @@
+package pages;
+
+import uistore.WatchesLocators;
+import utils.Assertion;
+import utils.Base;
+import utils.Screenshot;
+import utils.WebDriverHelper;
+
+public class WatchesPage {
+    public WebDriverHelper helper;
+    public WatchesPage(){
+        helper=new WebDriverHelper(Base.driver);
+    }
+
+    public void acceptOnCookies(){
+        try {
+            helper.waitForElementToBeVisible(WatchesLocators.acceptOnCookies,5);
+            helper.clickOnElement(WatchesLocators.acceptOnCookies);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void hoverOnWatches(){
+        try {
+            acceptOnCookies();
+            helper.waitForElementToBeVisible(WatchesLocators.hoverOnWatches,5);
+            helper.hoverOverElement(WatchesLocators.hoverOnWatches);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void clickOnBestSellers(){
+        try {
+            helper.waitForElementToBeVisible(WatchesLocators.clickOnBestSellers,5);
+            helper.clickOnElement(WatchesLocators.clickOnBestSellers);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // public void verifyContains(){
+    //     Assertion assertion = new Assertion(Base.driver);
+    //     assertion.verifyTitleOfPage("Best Sellers");
+    // }
+
+    public void clickOnCross(){
+        try {
+            helper.waitForElementToBeVisible(WatchesLocators.clickOnCross,5);
+            helper.clickOnElement(WatchesLocators.clickOnCross);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void clickOnForHim(){
+        try {
+            try {
+                clickOnCross();
+            } catch (Exception e) {
+            }
+            helper.waitForElementToBeVisible(WatchesLocators.clickOnForHim,5);
+            helper.clickOnElement(WatchesLocators.clickOnForHim);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clickOnCartier(){
+        try {
+            helper.waitForElementToBeVisible(WatchesLocators.clickOnCartier,5);
+            helper.clickOnElement(WatchesLocators.clickOnCartier);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clickOnDialColor(){
+        try {
+            helper.waitForElementToBeVisible(WatchesLocators.clickOnDialColor,5);
+            helper.clickOnElement(WatchesLocators.clickOnDialColor);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clickOnBlack(){
+        try {
+            helper.waitForElementToBeVisible(WatchesLocators.clickOnBlack,5);
+            helper.clickOnElement(WatchesLocators.clickOnBlack);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clickOnFirstProduct(){
+        try {
+            helper.waitForElementToBeVisible(WatchesLocators.clickOnFirstProduct,5);
+            helper.clickOnElement(WatchesLocators.clickOnFirstProduct);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clickOnAdd(){
+        try {
+            helper.waitForElementToBeVisible(WatchesLocators.clickOnAdd,5);
+            helper.clickOnElement(WatchesLocators.clickOnAdd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testcase1(){
+        hoverOnWatches();
+        clickOnBestSellers();
+       // verifyContains();
+        clickOnForHim();
+        clickOnCartier();
+        clickOnDialColor();
+        clickOnBlack();
+        clickOnFirstProduct();
+        clickOnAdd();
+
+    }
+
+    
+
+
+
+
+
+
+    
+}
--- Project/src/test/java/runner/TestRunner.java
+++ Project/src/test/java/runner/TestRunner.java
@@ -1,6 +1,25 @@
 package runner;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class TestRunner {
-    
+import pages.WatchesPage;
+import utils.Base;
+
+public class TestRunner extends Base {
+
+    @BeforeMethod
+    public void open(){
+        openBrowser();
+    }
+    @AfterMethod
+    public void close(){
+        driver.quit();
+    }
+    @Test
+    public void case1(){
+        WatchesPage obj1=new WatchesPage();
+        obj1.testcase1();
+    }
 }
