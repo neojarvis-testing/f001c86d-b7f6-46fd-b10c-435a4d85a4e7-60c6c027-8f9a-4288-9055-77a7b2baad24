a961f62a5b1a14aef1f1215a8a19109c8cd6b2ce
sumayyasultana5 <sumaayasultana898@gmail.com>
2025-04-21 17:15:59 +0000

    Second Commit Sumayya

--- Project/src/test/java/runner/TestRunner.java
+++ Project/src/test/java/runner/TestRunner.java
@@ -1,5 +1,15 @@
 package runner;
 
-public class TestRunner {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import utils.Base;
+
+public class TestRunner extends Base{
+
+    @Test
+    public void launch(){
+        openBrowser();
+    }
 }
