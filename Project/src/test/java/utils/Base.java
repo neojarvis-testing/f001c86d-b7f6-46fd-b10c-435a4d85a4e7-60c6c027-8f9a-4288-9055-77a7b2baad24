2177fae45975ee344740d1bbadd4963cc455607a
sumayyasultana5 <sumaayasultana898@gmail.com>
2025-04-23 06:06:49 +0000

    sumayya commited

--- Project/src/test/java/utils/Base.java
+++ Project/src/test/java/utils/Base.java
@@ -1,14 +1,11 @@
 package utils;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
